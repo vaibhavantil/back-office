2c3ecc0775397641abb02c4d399ff727fa36aa1d
Victor Smirnov <victor.smirnov@fastdev.se>
2018-01-12 18:02:56 +0500

    Create user only if it does not exist.

--- src/main/java/com/hedvig/backoffice/services/DatabaseLoader.java
+++ src/main/java/com/hedvig/backoffice/services/DatabaseLoader.java
@@ -1,12 +1,11 @@
 package com.hedvig.backoffice.services;
 
 import com.hedvig.backoffice.domain.Personnel;
+import com.hedvig.backoffice.repository.PersonnelRepository;
 import lombok.val;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
-
-import javax.persistence.EntityManager;
 
 /**
  * Prepare test data, this is only needed in development.
@@ -14,18 +13,20 @@ import javax.persistence.EntityManager;
 
 @Component
 public class DatabaseLoader {
+    private final PersonnelRepository personnelRepository;
 
-    private final EntityManager entityManager;
     private final PasswordEncoder passwordEncoder;
 
-    public DatabaseLoader(EntityManager entityManager, PasswordEncoder passwordEncoder) {
-        this.entityManager = entityManager;
+    public DatabaseLoader(PersonnelRepository personnelRepository, PasswordEncoder passwordEncoder) {
+        this.personnelRepository = personnelRepository;
         this.passwordEncoder = passwordEncoder;
     }
 
     @Transactional
     public void createUser() {
-        val login = new Personnel("victor@hedvig.com", passwordEncoder.encode("123"));
-        entityManager.persist(login);
+        val email = "victor@hedvig.com";
+        if (!personnelRepository.findByEmail(email).isPresent()) {
+            personnelRepository.save(new Personnel(email, passwordEncoder.encode("123")));
+        }
     }
 }
