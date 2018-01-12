b28aa5f480dc78a9ce0dc6fe1776330fc48bb9c4
Anton Abyshev <anton.abyshev@fastdev.se>
2018-01-12 15:27:16 +0400

    test UserService only for UserServiceImpl

--- src/main/java/com/hedvig/backoffice/BackOfficeStartupApplicationListener.java
+++ src/main/java/com/hedvig/backoffice/BackOfficeStartupApplicationListener.java
@@ -7,6 +7,7 @@ import com.hedvig.backoffice.services.users.UserServiceException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.stereotype.Component;
@@ -17,29 +18,34 @@ public class BackOfficeStartupApplicationListener implements ApplicationListener
     private static Logger logger = LoggerFactory.getLogger(BackOfficeStartupApplicationListener.class);
 
     private final UserService userService;
+    private boolean userServiceStub;
 
     @Autowired
-    public BackOfficeStartupApplicationListener(UserService userService) {
+    public BackOfficeStartupApplicationListener(UserService userService,
+                                                @Value("${userservice.stub:false}") boolean userServiceStub) {
         this.userService = userService;
+        this.userServiceStub = userServiceStub;
     }
 
     @Override
     public void onApplicationEvent(ContextRefreshedEvent event) {
-        try {
-            logger.info("TEST USER SERVICE:");
-            ObjectMapper mapper = new ObjectMapper();
-            userService.list()
-                    .stream()
-                    .limit(10)
-                    .forEach(u -> {
-                        try {
-                            logger.info(mapper.writerWithDefaultPrettyPrinter().writeValueAsString(u));
-                        } catch (JsonProcessingException e) {
-                            e.printStackTrace();
-                        }
-                    });
-        } catch (UserServiceException e) {
-            e.printStackTrace();
+        if (!userServiceStub) {
+            try {
+                logger.info("TEST USER SERVICE:");
+                ObjectMapper mapper = new ObjectMapper();
+                userService.list()
+                        .stream()
+                        .limit(10)
+                        .forEach(u -> {
+                            try {
+                                logger.info(mapper.writerWithDefaultPrettyPrinter().writeValueAsString(u));
+                            } catch (JsonProcessingException e) {
+                                e.printStackTrace();
+                            }
+                        });
+            } catch (UserServiceException e) {
+                e.printStackTrace();
+            }
         }
     }
 
