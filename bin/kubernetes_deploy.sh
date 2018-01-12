b59f5c1942032d77173864d40561df554b0fa93d
Victor Smirnov <victor.smirnov@fastdev.se>
2018-01-12 13:14:12 +0500

    Fixed dependency version and added kubernetes deployment script.

---
+++ bin/kubernetes_deploy.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+aws s3 cp s3://dev-com-hedvig-cluster-ett-data/kube ~/.kube --recursive
+
+curl -LO https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl
+chmod +x ./kubectl
+
+./kubectl set image deployment/back-office-service back-office-service=$REMOTE_IMAGE_URL:$TRAVIS_BUILD_NUMBER
