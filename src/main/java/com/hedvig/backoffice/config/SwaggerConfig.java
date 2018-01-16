5d96d8a567fc3c28f561281db7c87cebfb84178c
Anton Abyshev <anton.abyshev@fastdev.se>
2018-01-16 11:43:49 +0400

    add swagger

---
+++ src/main/java/com/hedvig/backoffice/config/SwaggerConfig.java
@@ -0,0 +1,30 @@
+package com.hedvig.backoffice.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiKey;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.Collections;
+
+@Configuration
+@Profile("development")
+@EnableSwagger2
+public class SwaggerConfig {
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build()
+                .securitySchemes(Collections.singletonList(new ApiKey("authkey", "Authorization", "header")));
+    }
+
+}
