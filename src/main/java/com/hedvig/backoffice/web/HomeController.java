b77b2604a2349eb0fa01e6b8b90e1e6d1be5da04
Anton Abyshev <anton.abyshev@fastdev.se>
2018-01-16 11:01:50 +0400

    return index page for all routes

--- src/main/java/com/hedvig/backoffice/web/HomeController.java
+++ src/main/java/com/hedvig/backoffice/web/HomeController.java
@@ -1,7 +1,18 @@
 package com.hedvig.backoffice.web;
 
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HomeController {
+
+    @RequestMapping(value = { "/", "/login", "/assets", "/messages/**" })
+    @ResponseBody
+    public Resource index() {
+        return new ClassPathResource("static/index.html");
+    }
+
 }
