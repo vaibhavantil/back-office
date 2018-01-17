8e68d47280064750ba5d1c70ea9d587f0108b087
Anton Abyshev <anton.abyshev@fastdev.se>
2018-01-17 10:55:36 +0400

    enable cors for websocket

--- src/main/java/com/hedvig/backoffice/config/WebSocketConfig.java
+++ src/main/java/com/hedvig/backoffice/config/WebSocketConfig.java
@@ -1,5 +1,6 @@
 package com.hedvig.backoffice.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.security.config.annotation.web.messaging.MessageSecurityMetadataSourceRegistry;
@@ -10,6 +11,12 @@ import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 @Configuration
 @EnableWebSocketMessageBroker
 public class WebSocketConfig extends AbstractSecurityWebSocketMessageBrokerConfigurer {
+
+    private String[] corsOrigins;
+
+    public WebSocketConfig(@Value("${cors.origins}") String[] corsOrigins) {
+        this.corsOrigins = corsOrigins;
+    }
 
     @Override
     protected void configureInbound(MessageSecurityMetadataSourceRegistry messages) {
@@ -26,8 +33,7 @@ public class WebSocketConfig extends AbstractSecurityWebSocketMessageBrokerConfi
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/chat");
-        registry.addEndpoint("/chat").withSockJS();
+        registry.addEndpoint("/chat").setAllowedOrigins(corsOrigins).withSockJS();
     }
 
     @Override
