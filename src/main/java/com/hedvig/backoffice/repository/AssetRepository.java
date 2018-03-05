0df5599c9b37d438ab3aade492552601dfd26fbf
Victor Smirnov <victor.smirnov@fastdev.se>
2018-03-05 19:22:57 +0500

    Save only newly added/updated assets. Count only pending assets.

--- src/main/java/com/hedvig/backoffice/repository/AssetRepository.java
+++ src/main/java/com/hedvig/backoffice/repository/AssetRepository.java
@@ -1,6 +1,7 @@
 package com.hedvig.backoffice.repository;
 
 import com.hedvig.backoffice.domain.Asset;
+import com.hedvig.common.constant.AssetState;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -16,4 +17,6 @@ public interface AssetRepository extends JpaRepository<Asset, String> {
 
     @Query("select a from Asset a where a.id in (:ids)")
     List<Asset> findAssetsById(@Param("ids") List<String> ids);
+
+    Integer countAllByState(@Param("state") AssetState state);
 }
