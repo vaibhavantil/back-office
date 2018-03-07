fd35b3cea04d31e4dc231d702cbc20950c738dea
Victor Smirnov <victor.smirnov@fastdev.se>
2018-03-07 13:42:56 +0500

    Cleaned the code and fixed formatting.

--- src/main/java/com/hedvig/backoffice/services/chat/ChatService.java
+++ src/main/java/com/hedvig/backoffice/services/chat/ChatService.java
@@ -7,12 +7,17 @@ import com.hedvig.backoffice.services.messages.dto.BotMessage;
 public interface ChatService {
 
     void send(String hid, Message message);
+
     void append(String hid, String message, String personnel);
-    void append(String hid, BotMessage message, String personnel);
+
     void messages(String hid);
+
     void messages(String hid, int count);
+
     void close(String sessionId);
+
     void subscribe(String hid, String subId, String sessionId, String principal);
+
     void unsubscribe(String subId, String sessionId);
 
     String getTopicPrefix();
--- src/main/java/com/hedvig/backoffice/services/messages/dto/BotMessage.java
+++ src/main/java/com/hedvig/backoffice/services/messages/dto/BotMessage.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 import com.hedvig.backoffice.services.messages.BotMessageException;
+import lombok.Getter;
 import org.apache.commons.lang3.StringUtils;
 
 import java.io.IOException;
@@ -11,16 +12,25 @@ import java.time.Instant;
 import java.time.format.DateTimeParseException;
 import java.util.Optional;
 
+@Getter
 public class BotMessage {
 
-    private JsonNode root;
+    private JsonNode message;
+
     private Instant timestamp;
+
     private Long globalId;
+
     private Long messageId;
+
     private JsonNode body;
+
     private JsonNode header;
+
     private String type;
+
     private String id;
+
     private String hid;
 
     public BotMessage(String message) throws BotMessageException {
@@ -30,7 +40,7 @@ public class BotMessage {
     public BotMessage(String message, boolean newMessage) throws BotMessageException {
         ObjectMapper mapper = new ObjectMapper();
         try {
-            this.root = mapper.readValue(message, JsonNode.class);
+            this.message = mapper.readValue(message, JsonNode.class);
         } catch (IOException e) {
             throw new BotMessageException(e);
         }
@@ -38,41 +48,19 @@ public class BotMessage {
         parseFields(newMessage);
     }
 
-    public BotMessage(JsonNode root) throws BotMessageException {
-        this(root, false);
+    public BotMessage(JsonNode message) throws BotMessageException {
+        this(message, false);
     }
 
-    public BotMessage(JsonNode root, boolean newMessage) throws BotMessageException {
-        this.root = root;
+    public BotMessage(JsonNode message, boolean newMessage) throws BotMessageException {
+        this.message = message;
         parseFields(newMessage);
     }
 
-    public Instant getTimestamp() {
-        return timestamp;
-    }
-
-    public Long getGlobalId() {
-        return globalId;
-    }
-
-    public JsonNode getMessage() {
-        return root;
-    }
-
-    public String getType() {
-        return type;
-    }
-
-    public JsonNode getBody() {
-        return body;
-    }
-
-    public JsonNode getHeader() {
-        return header;
-    }
-
-    public Long getMessageId() {
-        return messageId;
+    public void setGlobalId(Long globalId) {
+        this.globalId = globalId;
+        ObjectNode root = (ObjectNode) this.message;
+        root.put("globalId", globalId);
     }
 
     public void setMessageId(Long messageId) {
@@ -81,29 +69,15 @@ public class BotMessage {
         header.put("messageId", messageId);
     }
 
-    public void setGlobalId(Long globalId) {
-        this.globalId = globalId;
-        ObjectNode root = (ObjectNode) this.root;
-        root.put("globalId", globalId);
-    }
-
-    public String getId() {
-        return id;
-    }
-
-    public String getHid() {
-        return hid;
-    }
-
     public void setHid(String hid) {
         this.hid = hid;
     }
 
     private void parseFields(boolean newMessage) throws BotMessageException {
-        header = Optional.ofNullable(root.get("header"))
+        header = Optional.ofNullable(message.get("header"))
                 .orElseThrow(() -> new BotMessageException("message must contains header"));
 
-        body = Optional.ofNullable(root.get("body"))
+        body = Optional.ofNullable(message.get("body"))
                 .orElseThrow(() -> new BotMessageException("message must contains body"));
 
         type = Optional.ofNullable(body.get("type"))
@@ -111,7 +85,7 @@ public class BotMessage {
                 .orElseThrow(() -> new BotMessageException("message must contains type"));
 
         if (!newMessage) {
-            globalId = Optional.ofNullable(root.get("globalId"))
+            globalId = Optional.ofNullable(message.get("globalId"))
                     .orElseThrow(() -> new BotMessageException("message must contains globalId"))
                     .asLong();
 
@@ -119,12 +93,12 @@ public class BotMessage {
                     .orElseThrow(() -> new BotMessageException("message must contains globalId"))
                     .asLong();
 
-            id = Optional.ofNullable(root.get("id"))
+            id = Optional.ofNullable(message.get("id"))
                     .map(JsonNode::asText)
                     .orElse("");
         }
 
-        JsonNode value = Optional.ofNullable(root.get("timestamp"))
+        JsonNode value = Optional.ofNullable(message.get("timestamp"))
                 .orElseThrow(() -> new BotMessageException("message must contains timestamp"));
 
         String timeStr = value.asText();
