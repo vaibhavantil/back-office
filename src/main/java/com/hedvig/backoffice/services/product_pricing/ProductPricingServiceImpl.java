ac36f468aa392a4212c8784f0d567aaa3b014a95
Anton Abyshev <anton.abyshev@fastdev.se>
2018-03-20 12:44:01 +0400

    add stub for pricing-service

--- src/main/java/com/hedvig/backoffice/services/product_pricing/ProductPricingServiceImpl.java
+++ src/main/java/com/hedvig/backoffice/services/product_pricing/ProductPricingServiceImpl.java
@@ -2,9 +2,7 @@ package com.hedvig.backoffice.services.product_pricing;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
 
-@Service
 public class ProductPricingServiceImpl implements ProductPricingService {
 
     private ProductPricingClient client;
