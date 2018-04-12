7585d6f1972dc89d05d7395c18ebdd91d13127ed
Anton Abyshev <anton.abyshev@fastdev.se>
2018-04-12 16:29:22 +0400

    refactor chat update service

--- src/main/java/com/hedvig/backoffice/services/chat/ChatUpdatesServiceImpl.java
+++ src/main/java/com/hedvig/backoffice/services/chat/ChatUpdatesServiceImpl.java
@@ -10,22 +10,24 @@ import com.hedvig.backoffice.services.messages.dto.BackOfficeMessage;
 import com.hedvig.backoffice.services.messages.dto.BotMessage;
 import com.hedvig.backoffice.services.questions.QuestionService;
 import com.hedvig.backoffice.services.settings.SystemSettingsService;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
-import java.io.UnsupportedEncodingException;
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
 import java.time.Instant;
 import java.util.*;
-import java.util.stream.Collectors;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
+@Slf4j
 @Service
 public class ChatUpdatesServiceImpl implements ChatUpdatesService {
-
-    private static Logger logger = LoggerFactory.getLogger(ChatUpdatesServiceImpl.class);
 
     private final ChatService chatService;
     private final BotService botService;
@@ -34,12 +36,14 @@ public class ChatUpdatesServiceImpl implements ChatUpdatesService {
 
     private final Set<String> questionId;
 
+    private ExecutorService executor;
+
     @Autowired
     public ChatUpdatesServiceImpl(ChatService chatService,
                                   BotService botService,
                                   SystemSettingsService systemSettingsService,
                                   QuestionService questionService,
-                                  @Value("${botservice.questionId}") String[] questionId) throws UnsupportedEncodingException {
+                                  @Value("${botservice.questionId}") String[] questionId) {
 
         this.chatService = chatService;
         this.botService = botService;
@@ -47,8 +51,22 @@ public class ChatUpdatesServiceImpl implements ChatUpdatesService {
         this.questionService = questionService;
         this.questionId = Sets.newHashSet(questionId);
 
-        logger.info("CHAT UPDATE SERVICE: ");
-        logger.info("question ids: " + Arrays.toString(questionId));
+        log.info("CHAT UPDATE SERVICE: ");
+        log.info("question ids: " + Arrays.toString(questionId));
+    }
+
+    @PostConstruct
+    public void setup() {
+        executor = Executors.newCachedThreadPool();
+    }
+
+    @PreDestroy
+    public void destroy() throws InterruptedException {
+        executor.shutdown();
+        boolean shutdownResult = executor.awaitTermination(10, TimeUnit.SECONDS);
+        if (!shutdownResult) {
+            log.error("Thread pool is not shutting down");
+        }
     }
 
     @Scheduled(fixedDelayString = "${intervals.chat}")
@@ -60,43 +78,55 @@ public class ChatUpdatesServiceImpl implements ChatUpdatesService {
             return;
         }
 
-        logger.info("bot-service: fetched " + fetched.size() + " messages");
+        Instant lastTimestamp = null;
+        Map<String, List<BotMessage>> messages = new HashMap<>();
+        List<BotMessage> questions = new ArrayList<>();
 
-        List<BotMessage> messages = fetched.stream().map(f -> {
+        for (BackOfficeMessage backOfficeMessage : fetched) {
+            BotMessage message;
+
             try {
-                return f.toBotMessage();
+                message = backOfficeMessage.toBotMessage();
             } catch (BotMessageException e) {
-                logger.error("Error during parsing message from bot-service", e);
+                log.error("Error during parsing message from bot-service", e);
+                continue;
             }
-            return null;
-        }).filter(Objects::nonNull).collect(Collectors.toList());
 
-        HashMap<String, Long> idStatistics = new HashMap<>();
-        for (BotMessage m : messages) {
-            Long count = idStatistics.getOrDefault(m.getId(), 0L) + 1;
-            idStatistics.put(m.getId(), count);
+            List<BotMessage> messagesForHid = messages.computeIfAbsent(backOfficeMessage.getUserId(), key -> new ArrayList<>());
+            messagesForHid.add(message);
+
+            if (questionId.contains(message.getId()) && !message.isBotMessage()) {
+                questions.add(message);
+            }
+
+            if (lastTimestamp == null || lastTimestamp.isBefore(message.getTimestamp())) {
+                lastTimestamp = message.getTimestamp();
+            }
         }
 
-        logger.info(idStatistics.toString());
+        if (lastTimestamp != null) {
+            systemSettingsService.update(SystemSettingType.BOT_SERVICE_LAST_TIMESTAMP, lastTimestamp.plusMillis(1).toString());
+        }
 
-        Map<String, List<BotMessage>> updates = messages.stream()
-                .collect(Collectors.groupingBy(BotMessage::getHid));
+        if (log.isDebugEnabled()) {
+            log.info("bot-service: fetched " + fetched.size() + " messages and " + questions.size() + " questions");
+        }
 
-        Instant lastTimestamp = messages.stream()
-                .max(Comparator.comparing(BotMessage::getTimestamp))
-                .map(BotMessage::getTimestamp)
-                .orElse(new Date().toInstant());
+        if (questions.size() > 0) {
+            CompletableFuture
+                    .runAsync(() -> addQuestions(questions), executor)
+                    .exceptionally(e -> {
+                        log.error("error while adding questions", e);
+                        return null;
+                    });
+        }
 
-        systemSettingsService.update(SystemSettingType.BOT_SERVICE_LAST_TIMESTAMP, lastTimestamp.plusMillis(1).toString());
-
-        updates.forEach((k, v) -> chatService.send(k, Message.chat(v)));
-
-        List<BotMessage> questions = messages.stream()
-                .filter(m -> questionId.contains(m.getId()) && !m.isBotMessage())
-                .collect(Collectors.toList());
-
-        logger.info("fetched questions: " + questions.size());
-        questionService.addNewQuestions(questions);
+        messages.forEach((k, v) -> CompletableFuture
+                .runAsync(() -> sendMessages(k, v))
+                .exceptionally(e -> {
+                    log.error("error while message sending", e);
+                    return null;
+                }));
     }
 
     private Instant lastTimestamp() {
@@ -105,5 +135,13 @@ public class ChatUpdatesServiceImpl implements ChatUpdatesService {
                 new Date().toInstant().toString());
 
         return Instant.parse(setting.getValue());
+    }
+
+    private void addQuestions(List<BotMessage> questions) {
+        questionService.addNewQuestions(questions);
+    }
+
+    private void sendMessages(String hid, List<BotMessage> messages) {
+        chatService.send(hid, Message.chat(messages));
     }
 }
