a81e5f5e2ed8b9768010dd572af905dff8938471
Anton Abyshev <anton.abyshev@fastdev.se>
2018-04-20 14:44:30 +0400

    do not update claims if count not changed

--- src/main/java/com/hedvig/backoffice/services/claims/ClaimsUpdates.java
+++ src/main/java/com/hedvig/backoffice/services/claims/ClaimsUpdates.java
@@ -7,23 +7,32 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
+import java.util.concurrent.atomic.AtomicLong;
+
 @Component
 public class ClaimsUpdates {
 
     private final ClaimsService claimsService;
     private final UpdatesService updatesService;
     private final SystemSettingsService settingsService;
+    private AtomicLong total;
 
     @Autowired
     public ClaimsUpdates(ClaimsService claimsService, UpdatesService updatesService, SystemSettingsService settingsService) {
         this.claimsService = claimsService;
         this.updatesService = updatesService;
         this.settingsService = settingsService;
+        this.total = new AtomicLong(0);
     }
 
     @Scheduled(fixedRateString = "${intervals.claims}")
     public void update() {
-        updatesService.set(claimsService.totalClaims(settingsService.getInternalAccessToken()), UpdateType.CLAIMS);
+        long current = total.get();
+        long updated = claimsService.totalClaims(settingsService.getInternalAccessToken());
+        if (current != updated) {
+            total.set(updated);
+            updatesService.set(updated, UpdateType.CLAIMS);
+        }
     }
 
 }
