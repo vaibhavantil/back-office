f295bb4bc9afb1d231a4a182302311b627a61fcb
meletios <meletios@hedvig.com>
2018-05-24 10:43:29 +0200

    add the edit member functionallity (front-end part)

--- src/main/js/app/store/actions/messagesActions.js
+++ src/main/js/app/store/actions/messagesActions.js
@@ -1,51 +1,63 @@
 import {
-    ADD_MESSAGE,
-    MESSAGE_RECEIVED,
-    CLEAR_MESSAGES_LIST,
-    SET_ACTIVE_CONNECTION,
-    ERROR_RECEIVED,
-    MEMBER_REQUESTING,
-    MEMBER_REQUEST_SUCCESS,
-    MEMBER_REQUEST_ERROR
-} from '../constants/members';
+  ADD_MESSAGE,
+  MESSAGE_RECEIVED,
+  CLEAR_MESSAGES_LIST,
+  SET_ACTIVE_CONNECTION,
+  ERROR_RECEIVED,
+  MEMBER_REQUESTING,
+  MEMBER_REQUEST_SUCCESS,
+  MEMBER_REQUEST_ERROR,
+  EDIT_MEMBER_DETAILS,
+  EDIT_MEMBER_DETAILS_SUCCESS
+} from "../constants/members";
 
 export const addMessage = (message, id, socket) => ({
-    type: ADD_MESSAGE,
-    message,
-    id,
-    socket
+  type: ADD_MESSAGE,
+  message,
+  id,
+  socket
 });
 
 export const messageReceived = message => ({
-    type: MESSAGE_RECEIVED,
-    message
+  type: MESSAGE_RECEIVED,
+  message
 });
 
 export const errorReceived = error => ({
-    type: ERROR_RECEIVED,
-    error
+  type: ERROR_RECEIVED,
+  error
 });
 
 export const clearMessagesList = () => ({
-    type: CLEAR_MESSAGES_LIST
+  type: CLEAR_MESSAGES_LIST
 });
 
 export const setActiveConnection = connection => ({
-    type: SET_ACTIVE_CONNECTION,
-    connection
+  type: SET_ACTIVE_CONNECTION,
+  connection
 });
 
 export const memberRequest = id => ({
-    type: MEMBER_REQUESTING,
-    id
+  type: MEMBER_REQUESTING,
+  id
 });
 
 export const memberRequestSuccess = member => ({
-    type: MEMBER_REQUEST_SUCCESS,
-    member
+  type: MEMBER_REQUEST_SUCCESS,
+  member
 });
 
 export const memberRequestError = error => ({
-    type: MEMBER_REQUEST_ERROR,
-    error
+  type: MEMBER_REQUEST_ERROR,
+  error
+});
+
+export const editMemberDetails = member => ({
+  type: EDIT_MEMBER_DETAILS,
+  member
+});
+
+export const editMemberDetailsSuccess = member => ({
+  type: EDIT_MEMBER_DETAILS_SUCCESS,
+  member
 });
--- src/main/js/app/store/sagas/messagesSaga.js
+++ src/main/js/app/store/sagas/messagesSaga.js
@@ -1,41 +1,58 @@
-import { call, takeEvery, put, takeLatest } from 'redux-saga/effects';
-import { MEMBER_REQUESTING, ADD_MESSAGE } from 'constants/members';
-import api from 'app/api';
-import config from 'app/api/config';
+import { call, takeEvery, put, takeLatest } from "redux-saga/effects";
 import {
-    memberRequestSuccess,
-    memberRequestError
-} from '../actions/messagesActions';
-import { showNotification } from '../actions/notificationsActions';
+  MEMBER_REQUESTING,
+  ADD_MESSAGE,
+  EDIT_MEMBER_DETAILS
+} from "constants/members";
+import api from "app/api";
+import config from "app/api/config";
+import {
+  memberRequestSuccess,
+  memberRequestError,
+  editMemberDetailsSuccess
+} from "../actions/messagesActions";
+import { showNotification } from "../actions/notificationsActions";
 
 function* memberRequestFlow({ id }) {
-    try {
-        const member = yield call(api, config.members.findOne, null, id);
-        yield put(memberRequestSuccess(member.data));
-    } catch (error) {
-        yield [
-            put(
-                showNotification({ message: error.message, header: 'Members' })
-            ),
-            put(memberRequestError(error))
-        ];
-    }
+  try {
+    const member = yield call(api, config.members.findOne, null, id);
+    yield put(memberRequestSuccess(member.data));
+  } catch (error) {
+    yield [
+      put(showNotification({ message: error.message, header: "Members" })),
+      put(memberRequestError(error))
+    ];
+  }
+}
+
+function* editMemberDetailsFlow({ member }) {
+  try {
+    const path = `${member.memberId}/edit`;
+    yield call(api, config.members.edit, member, path);
+    yield put(editMemberDetailsSuccess(member));
+  } catch (error) {
+    yield [
+      put(memberRequestError(error)),
+      put(showNotification({ message: error.message, header: "Member" }))
+    ];
+  }
 }
 
 function* messagesWatcher() {
-    yield [
-        takeEvery(ADD_MESSAGE, ({ message, socket, id }) => {
-            socket.send(
-                config.ws.send + id,
-                {},
-                JSON.stringify({
-                    memberId: id,
-                    msg: message
-                })
-            );
-        }),
-        takeLatest(MEMBER_REQUESTING, memberRequestFlow)
-    ];
+  yield [
+    takeEvery(ADD_MESSAGE, ({ message, socket, id }) => {
+      socket.send(
+        config.ws.send + id,
+        {},
+        JSON.stringify({
+          memberId: id,
+          msg: message
+        })
+      );
+    }),
+    takeLatest(MEMBER_REQUESTING, memberRequestFlow),
+    takeLatest(EDIT_MEMBER_DETAILS, editMemberDetailsFlow)
+  ];
 }
 
 export default messagesWatcher;
