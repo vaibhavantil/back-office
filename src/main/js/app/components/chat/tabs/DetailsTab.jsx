6f183d90c6a353653a11b07bd05d603a8aece8f1
meletios <meletios@hedvig.com>
2018-05-30 14:06:01 +0200

    disable registredOn field from editing

--- src/main/js/app/components/chat/tabs/DetailsTab.jsx
+++ src/main/js/app/components/chat/tabs/DetailsTab.jsx
@@ -23,6 +23,7 @@ export default class DetailsTab extends React.Component {
       case "status":
       case "ssn":
       case "birthdate":
+      case "registeredon":
         return true;
       default:
         return false;
