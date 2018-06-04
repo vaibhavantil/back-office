e0a9bdef8854a7f7c331f6ef581383068e5f2a70
meletios <meletios@hedvig.com>
2018-06-04 16:05:08 +0200

    fix formating of the date

--- src/main/js/app/components/members/members-list/MembersList.jsx
+++ src/main/js/app/components/members/members-list/MembersList.jsx
@@ -28,12 +28,12 @@ export default class MembersList extends React.Component {
   getTableRow = item => {
     const signedOn = moment(item.signedOn);
     const formattedsignedOn = signedOn.isValid()
-      ? signedOn.format("DD MMMM YYYY HH:MM")
+      ? signedOn.format("DD MMMM YYYY HH:mm")
       : "-";
 
     const createdOn = moment(item.createdOn);
     const formattedCreatedOn = createdOn.isValid()
-      ? createdOn.format("DD MMMM YYYY HH:MM")
+      ? createdOn.format("DD MMMM YYYY HH:mm")
       : "-";
 
     return (
