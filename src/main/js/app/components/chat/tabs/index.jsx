c8b4c51b5d302e74730efe3b31fa0e68917459c0
meletios <meletios@hedvig.com>
2018-07-10 10:20:01 +0200

    add new tab with all the available insurances for a member

--- src/main/js/app/components/chat/tabs/index.jsx
+++ src/main/js/app/components/chat/tabs/index.jsx
@@ -1,63 +1,70 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import { Tab } from 'semantic-ui-react';
-import styled from 'styled-components';
-import ChatTab from './ChatTab';
-import DetailsTab from './DetailsTab';
-import ClaimsTab from './ClaimsTab';
-import InsuranceTab from './InsuranceTab';
+import React from "react";
+import PropTypes from "prop-types";
+import { Tab } from "semantic-ui-react";
+import styled from "styled-components";
+import ChatTab from "./ChatTab";
+import DetailsTab from "./DetailsTab";
+import ClaimsTab from "./ClaimsTab";
+import InsuranceTab from "./InsuranceTab";
+import InsuranceListTab from "./InsuranceListTab";
 
 const TabContainer = styled(Tab.Pane)`
-    &&& {
-        display: flex;
-        flex-direction: column;
-        min-width: 700px;
-        height: ${props => (props.isChatTab ? '100%' : 'auto')};
-        margin-bottom: 50px !important;
-    }
+  &&& {
+    display: flex;
+    flex-direction: column;
+    min-width: 700px;
+    height: ${props => (props.isChatTab ? "100%" : "auto")};
+    margin-bottom: 50px !important;
+  }
 `;
 
 const TabItem = ({ props, TabContent, isChatTab }) => (
-    <TabContainer isChatTab={isChatTab}>
-        <TabContent {...props} />
-    </TabContainer>
+  <TabContainer isChatTab={isChatTab}>
+    <TabContent {...props} />
+  </TabContainer>
 );
 
 TabItem.propTypes = {
-    props: PropTypes.object.isRequired,
-    TabContent: PropTypes.func,
-    isChatTab: PropTypes.bool
+  props: PropTypes.object.isRequired,
+  TabContent: PropTypes.func,
+  isChatTab: PropTypes.bool
 };
 
 /* eslint-disable react/display-name */
 const memberPagePanes = (props, addMessage, socket) => {
-    const { insurance } = props;
-    const panes = [
-        {
-            menuItem: 'Details',
-            render: () => <TabItem props={props} TabContent={DetailsTab} />
-        },
-        {
-            menuItem: 'Chat',
-            render: () => (
-                <TabItem
-                    TabContent={ChatTab}
-                    props={{ ...props, addMessage, socket }}
-                    isChatTab={true}
-                />
-            )
-        },
-        {
-            menuItem: 'Claims',
-            render: () => <TabItem props={props} TabContent={ClaimsTab} />
-        }
-    ];
-    if (!insurance.error.length && insurance.data) {
-        panes.push({
-            menuItem: 'Insurance',
-            render: () => <TabItem props={props} TabContent={InsuranceTab} />
-        });
+  const { insurance } = props;
+  const panes = [
+    {
+      menuItem: "Details",
+      render: () => <TabItem props={props} TabContent={DetailsTab} />
+    },
+    {
+      menuItem: "Chat",
+      render: () => (
+        <TabItem
+          TabContent={ChatTab}
+          props={{ ...props, addMessage, socket }}
+          isChatTab={true}
+        />
+      )
+    },
+    {
+      menuItem: "Claims",
+      render: () => <TabItem props={props} TabContent={ClaimsTab} />
     }
-    return panes;
+  ];
+  if (!insurance.error.length && insurance.data) {
+    panes.push({
+      menuItem: "Current Insurance",
+      render: () => <TabItem props={props} TabContent={InsuranceTab} />
+    });
+  }
+  if (!insurance.error.length && insurance.data) {
+    panes.push({
+      menuItem: "All Insurances",
+      render: () => <TabItem props={props} TabContent={InsuranceListTab} />
+    });
+  }
+  return panes;
 };
 export default memberPagePanes;
