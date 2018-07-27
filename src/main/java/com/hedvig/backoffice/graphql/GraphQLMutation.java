912ebce0a5f0033a39c5db01e78355469fb9d95b
Oscar Nylander <oscar@hedvig.com>
2018-07-27 12:47:54 +0200

    Log who makes charge attempts

--- src/main/java/com/hedvig/backoffice/graphql/GraphQLMutation.java
+++ src/main/java/com/hedvig/backoffice/graphql/GraphQLMutation.java
@@ -7,22 +7,38 @@ import javax.money.MonetaryAmount;
 import com.coxautodev.graphql.tools.GraphQLMutationResolver;
 import com.hedvig.backoffice.graphql.dataloaders.MemberLoader;
 import com.hedvig.backoffice.graphql.types.Member;
+import com.hedvig.backoffice.security.AuthorizationException;
 import com.hedvig.backoffice.services.payments.PaymentService;
+import com.hedvig.backoffice.services.personnel.PersonnelService;
 
 import org.springframework.stereotype.Component;
 
-@Component
-public class GraphQLMutation implements GraphQLMutationResolver {
-  private MemberLoader memberLoader;
-  private PaymentService paymentService;
+import graphql.schema.DataFetchingEnvironment;
+import lombok.val;
+import lombok.extern.slf4j.Slf4j;
 
-  public GraphQLMutation(PaymentService paymentService, MemberLoader memberLoader) {
+@Component
+@Slf4j
+public class GraphQLMutation implements GraphQLMutationResolver {
+  private final MemberLoader memberLoader;
+  private final PaymentService paymentService;
+  private final PersonnelService personnelService;
+
+  public GraphQLMutation(PaymentService paymentService, PersonnelService personnelService, MemberLoader memberLoader) {
     this.paymentService = paymentService;
+    this.personnelService = personnelService;
     this.memberLoader = memberLoader;
   }
 
-  public CompletableFuture<Member> chargeMember(String id, MonetaryAmount amount) {
+  public CompletableFuture<Member> chargeMember(String id, MonetaryAmount amount, DataFetchingEnvironment env) throws AuthorizationException {
+    log.info("Personnel with email '{}' attempting to charge member '{}' the amount '{}'", getEmail(env), id, amount.toString());
     paymentService.chargeMember(id, amount);
     return memberLoader.load(id);
+  }
+
+  private String getEmail(DataFetchingEnvironment env) throws AuthorizationException {
+    GraphQLRequestContext context = env.getContext();
+    val personnel = personnelService.getPersonnel(context.getUserPrincipal().getName());
+    return personnel.getEmail();
   }
 }
--- src/main/java/com/hedvig/backoffice/graphql/GraphQLRequestContextBuilder.java
+++ src/main/java/com/hedvig/backoffice/graphql/GraphQLRequestContextBuilder.java
@@ -1,18 +1,25 @@
 package com.hedvig.backoffice.graphql;
 
+import java.security.Principal;
 import java.util.Optional;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.stereotype.Component;
+
 import graphql.servlet.GraphQLContext;
 import graphql.servlet.GraphQLContextBuilder;
 
-public class ContextBuilder implements GraphQLContextBuilder {
+@Component
+public class GraphQLRequestContextBuilder implements GraphQLContextBuilder {
 
 	@Override
 	public GraphQLContext build(Optional<HttpServletRequest> req, Optional<HttpServletResponse> resp) {
-		// TODO Here we can get the information about who makes the request, so that we can attribute actions made by this person
-		return null;
+		Principal userPrincipal = null;
+		if (req.isPresent()) {
+			userPrincipal = req.get().getUserPrincipal();
+		}
+		return new GraphQLRequestContext(req, resp, userPrincipal);
 	}
 }
