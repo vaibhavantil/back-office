01f60bb64dad8022c780193bdfd0dd015768eb9b
meletios <meletios@hedvig.com>
2018-07-30 14:41:49 +0200

    add field id to TransactionDTO

--- src/main/java/com/hedvig/backoffice/services/payments/dto/TransactionDTO.java
+++ src/main/java/com/hedvig/backoffice/services/payments/dto/TransactionDTO.java
@@ -3,10 +3,12 @@ package com.hedvig.backoffice.services.payments.dto;
 import java.math.BigDecimal;
 import java.time.Instant;
 
+import java.util.UUID;
 import lombok.Value;
 
 @Value
 public class TransactionDTO {
+    UUID id;
     BigDecimal amount;
     String currency;
     Instant timestamp;
