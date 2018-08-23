521c0c6abec1db7b199b43c7fc0e483c687a3eff
meletios <meletios@hedvig.com>
2018-08-23 13:56:26 +0200

    delete old notification client

--- src/main/java/com/hedvig/backoffice/services/notification/NotificationService.java
+++
@@ -1,5 +0,0 @@
-package com.hedvig.backoffice.services.notification;
-
-public interface NotificationService {
-  void sendPushNotification(String memberId, String personnelToken);
-}
--- src/main/java/com/hedvig/backoffice/services/notification/dto/SendPushNotificationRequest.java
+++
@@ -1,8 +0,0 @@
-package com.hedvig.backoffice.services.notification.dto;
-
-import lombok.Value;
-
-@Value
-public class SendPushNotificationRequest {
- // TODO Add all needed data for sending a push notification to notificationservcie
-}
