24179407c86953876ceb555c5b9e93f01c2f28e2
meletios <meletios@hedvig.com>
2018-09-17 18:49:14 +0200

    add bdx to routes

--- src/main/js/app/routes/index.js
+++ src/main/js/app/routes/index.js
@@ -1,23 +1,25 @@
-import AssetsPageRoute from './AssetsPageRoute';
-import LoginPageRoute from './LoginPageRoute';
-import LoginProcessPageRoute from './LoginProcessPageRoute';
-import MembersPageRoute from './MembersPageRoute';
-import DashboardPageRoute from './DashboardPageRoute';
-import ClaimsPageRoute from './ClaimsPageRoute';
-import QuestionsPageRoute from './QuestionsPageRoute';
-import MemberInsurancePageRoute from './MemberInsurancePageRoute';
-import PaymentsPageRoute from './PaymentsPageRoute';
-import PrivateRoute from './PrivateRoute';
+import AssetsPageRoute from "./AssetsPageRoute";
+import LoginPageRoute from "./LoginPageRoute";
+import LoginProcessPageRoute from "./LoginProcessPageRoute";
+import MembersPageRoute from "./MembersPageRoute";
+import DashboardPageRoute from "./DashboardPageRoute";
+import ClaimsPageRoute from "./ClaimsPageRoute";
+import QuestionsPageRoute from "./QuestionsPageRoute";
+import MemberInsurancePageRoute from "./MemberInsurancePageRoute";
+import PaymentsPageRoute from "./PaymentsPageRoute";
+import BourdereauPageRoute from "./BourdereauPageRoute";
+import PrivateRoute from "./PrivateRoute";
 
 export default {
-    AssetsPageRoute,
-    LoginPageRoute,
-    LoginProcessPageRoute,
-    MembersPageRoute,
-    DashboardPageRoute,
-    ClaimsPageRoute,
-    QuestionsPageRoute,
-    MemberInsurancePageRoute,
-    PaymentsPageRoute,
-    PrivateRoute
+  AssetsPageRoute,
+  LoginPageRoute,
+  LoginProcessPageRoute,
+  MembersPageRoute,
+  DashboardPageRoute,
+  ClaimsPageRoute,
+  QuestionsPageRoute,
+  MemberInsurancePageRoute,
+  PaymentsPageRoute,
+  BourdereauPageRoute,
+  PrivateRoute
 };
