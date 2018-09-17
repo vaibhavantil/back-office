055df6ec05b97e6f783fa03f605fbf9b872d9f75
meletios <meletios@hedvig.com>
2018-09-17 18:46:34 +0200

    creating the csv file on backend

--- src/main/java/com/hedvig/backoffice/web/dto/BordereauCSVDTO.java
+++ src/main/java/com/hedvig/backoffice/web/dto/BordereauCSVDTO.java
@@ -1,5 +1,21 @@
 package com.hedvig.backoffice.web.dto;
 
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+import com.hedvig.backoffice.services.product_pricing.dto.MonthlyBordereauDTO;
+
+@JsonPropertyOrder(value = {"memberId", "expectedSubscription", "earnedSubscription",
+  "productType"})
 public class BordereauCSVDTO {
 
+  public String memberId;
+  public double expectedSubscription;
+  public double earnedSubscription;
+  public String productType;
+
+  public BordereauCSVDTO(MonthlyBordereauDTO b) {
+    this.memberId = b.getMemberId();
+    this.expectedSubscription = b.getExpectedSubscription().getNumber().doubleValueExact();
+    this.earnedSubscription = b.getEarnedSubscription().getNumber().doubleValueExact();
+    this.productType = b.getProductType();
+  }
 }
