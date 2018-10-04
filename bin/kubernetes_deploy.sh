63a0051b2c49a6cec5a72ba4cdfc9ef109f06a11
meletios <meletios@hedvig.com>
2018-10-04 13:15:39 +0200

    push and deploy images from develop

--- bin/kubernetes_deploy.sh
+++ bin/kubernetes_deploy.sh
@@ -3,7 +3,7 @@
 # Push only if it's not a pull request
 if [ -z "$TRAVIS_PULL_REQUEST" ] || [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
   # Push only if we're testing the master branch
-  if [ "$TRAVIS_BRANCH" == "master" ]; then
+  if [ "$TRAVIS_BRANCH" == "master" ] || [ "$TRAVIS_BRANCH" == "develop" ]; then
     aws s3 cp s3://dev-com-hedvig-cluster-ett-data/kube ~/.kube --recursive
     
     curl -LO https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl
@@ -12,7 +12,7 @@ if [ -z "$TRAVIS_PULL_REQUEST" ] || [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
     
     ./kubectl set image deployment/back-office-service back-office-service=$REMOTE_IMAGE_URL:${TRAVIS_COMMIT}
   else
-    echo "Skipping deploy because branch is not 'master'"
+    echo "Skipping deploy because branch is not 'master' or 'develop'"
   fi
 else
   echo "Skipping deploy because it's a pull request"
