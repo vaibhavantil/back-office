62bf26730069a9578a050a873eb9df21e45cc912
Oscar Nylander <oscar@hedvig.com>
2018-11-23 11:17:22 +0100

    Implement LocalDate-scalar

--- src/main/java/com/hedvig/backoffice/graphql/scalars/LocalDateScalar.java
+++ src/main/java/com/hedvig/backoffice/graphql/scalars/LocalDateScalar.java
@@ -2,6 +2,7 @@ package com.hedvig.backoffice.graphql.scalars;
 
 import java.time.LocalDate;
 import org.springframework.stereotype.Component;
+import graphql.language.StringValue;
 import graphql.schema.Coercing;
 import graphql.schema.CoercingParseLiteralException;
 import graphql.schema.CoercingParseValueException;
@@ -16,19 +17,36 @@ public class LocalDateScalar extends GraphQLScalarType {
 
           @Override
           public String serialize(Object dataFetcherResult) throws CoercingSerializeException {
-            return null;
+            if (dataFetcherResult == null) {
+              return null;
+            }
+
+            if (!(dataFetcherResult instanceof LocalDate)) {
+              throw new CoercingSerializeException(
+                  String.format("dataFetcherResult is of wrong type: Expected %s, got %s",
+                      LocalDate.class.toString(), dataFetcherResult.getClass().toString()));
+            }
+
+            return ((LocalDate) dataFetcherResult).toString();
           }
 
           @Override
           public LocalDate parseValue(Object input) throws CoercingParseValueException {
-            return null;
+            try {
+              return LocalDate.parse((String) input);
+            } catch (Exception e) {
+              throw new CoercingParseValueException("Could not parse value", e);
+            }
           }
 
           @Override
           public LocalDate parseLiteral(Object input) throws CoercingParseLiteralException {
-            return null;
+            try {
+              return LocalDate.parse(((StringValue) input).getValue());
+            } catch (Exception e) {
+              throw new CoercingParseLiteralException("Could not parse literal", e);
+            }
           }
-
         });
   }
 }
