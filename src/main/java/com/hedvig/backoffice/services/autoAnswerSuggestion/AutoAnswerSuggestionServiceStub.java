17370b4a25ddab37af88852bb564868029a06276
hkindbom <hannes.kindbom@hotmail.com>
2019-07-02 17:01:03 +0200

    parsing allReplies in backoffice

--- src/main/java/com/hedvig/backoffice/services/autoAnswerSuggestion/AutoAnswerSuggestionServiceStub.java
+++ src/main/java/com/hedvig/backoffice/services/autoAnswerSuggestion/AutoAnswerSuggestionServiceStub.java
@@ -2,11 +2,6 @@ package com.hedvig.backoffice.services.autoAnswerSuggestion;
 
 import com.hedvig.backoffice.services.autoAnswerSuggestion.SuggestionDTO.SuggestionDTO;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.boot.configurationprocessor.json.JSONException;
-import org.springframework.boot.configurationprocessor.json.JSONObject;
-import springfox.documentation.spring.web.json.Json;
-
-import java.util.ArrayList;
 import java.util.List;
 
 
@@ -73,16 +68,3 @@ public class AutoAnswerSuggestionServiceStub implements AutoAnswerSuggestionServ
   }
 
 }
-
-/*
-the below is the string returned from python API
-
-[
-  {
-  "intent": "next_fee_date",
-  "reply": "Pengarna dras fr\u00e5n ditt bankkonto via autogiro den 27e varje m\u00e5nad \ud83d\udcb8",
-  "text": "N\u00e4r ska n\u00e4sta faktura betalas?"
-  "allReplies": {}
-  }
-  ]
-*/
--- src/main/java/com/hedvig/backoffice/services/autoAnswerSuggestion/SuggestionDTO/SuggestionDTO.java
+++ src/main/java/com/hedvig/backoffice/services/autoAnswerSuggestion/SuggestionDTO/SuggestionDTO.java
@@ -4,32 +4,55 @@ import lombok.Value;
 import org.springframework.boot.configurationprocessor.json.JSONException;
 import org.springframework.boot.configurationprocessor.json.JSONObject;
 
+import java.util.ArrayList;
+import java.util.Iterator;
 
 public class SuggestionDTO {
 
   String intent;
   String reply;
   String text;
-  String allReplies;
+  ArrayList<ReplyEntry> allRepliesArray;
 
   public SuggestionDTO(String autoAnswerServiceReply) {
+
     //strip [ and ] in beginning and end of string
     String result = autoAnswerServiceReply.substring(1, autoAnswerServiceReply.length() - 2);
+
+    allRepliesArray = new ArrayList<>();
 
     try {
       JSONObject obj = new JSONObject(result);
       this.intent = obj.getString("intent");
       this.reply = obj.getString("reply");
       this.text = obj.getString("text");
-      this.allReplies = obj.getString("allReplies");
 
+      JSONObject allReplies = obj.getJSONObject("allReplies");
+      Iterator<String> keys = allReplies.keys();
+
+      while (keys.hasNext()) {
+        String key = keys.next();
+        if (allReplies.get(key) instanceof JSONObject) {
+
+          JSONObject replyObj = allReplies.getJSONObject(key);
+          allRepliesArray.add(new ReplyEntry(key, replyObj.getString("reply")));
+        }
+      }
 
     } catch (JSONException e) {
 
       e.printStackTrace();
     }
 
+  }
 
+  public class ReplyEntry {
+    String intent;
+    String reply;
 
+    public ReplyEntry(String intent, String reply) {
+      this.intent = intent;
+      this.reply = reply;
+    }
   }
 }
