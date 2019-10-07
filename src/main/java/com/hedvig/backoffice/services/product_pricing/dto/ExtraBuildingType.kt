794da153c82cdc1898e5f89dff2764517cffa60c
vonElfvin <elvin.granat@gmail.com>
2019-10-07 17:57:27 +0200

    Add more ExtraBuildingTypes

--- src/main/java/com/hedvig/backoffice/services/product_pricing/dto/ExtraBuildingType.kt
+++ src/main/java/com/hedvig/backoffice/services/product_pricing/dto/ExtraBuildingType.kt
@@ -2,7 +2,17 @@ package com.hedvig.backoffice.services.product_pricing.dto
 
 enum class ExtraBuildingType {
   GARAGE,
-  ATTEFALL,
+  CARPORT,
+  SHED,
+  STOREHOUSE,
   FRIGGEBOD,
+  ATTEFALL,
+  OUTHOUSE,
+  GUESTHOUSE,
+  GAZEBO,
+  GREENHOUSE,
+  SAUNA,
+  BARN,
+  BOATHOUSE,
   OTHER
 }
