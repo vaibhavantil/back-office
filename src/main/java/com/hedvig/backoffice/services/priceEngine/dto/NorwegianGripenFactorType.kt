9d43cd7b40e9e3eab0f6134fe5c5f1fc5754d875
vonElfvin <elvin.granat@gmail.com>
2020-03-19 17:55:35 +0100

    Rename RENT_OR_OWN to HOUSEHOLD_TYPE

--- src/main/java/com/hedvig/backoffice/services/priceEngine/dto/NorwegianGripenFactorType.kt
+++ src/main/java/com/hedvig/backoffice/services/priceEngine/dto/NorwegianGripenFactorType.kt
@@ -1,10 +1,11 @@
 package com.hedvig.backoffice.services.priceEngine.dto
 
 enum class NorwegianGripenFactorType {
-    AGE,
-    CENTRALITY_GROUP,
-    ECONOMY_OF_MUNICIPALITY,
-    NUMBER_OF_PEOPLE,
-    APARTMENT_SIZE,
-    RENTED_OR_OWNED
+  AGE,
+  CENTRALITY_GROUP,
+  ECONOMY_OF_MUNICIPALITY,
+  NUMBER_OF_PEOPLE,
+  APARTMENT_SIZE,
+  HOUSEHOLD_TYPE,
+  DEDUCTIBLE
 }
