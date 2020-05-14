8ebec65e875b541b394aa278bb22749f39fac217
meletios <meletios@hedvig.com>
2020-05-14 10:40:25 +0200

    add queries and mutations for creating and signing contracts

---
+++ src/main/java/com/hedvig/backoffice/graphql/resolvers/SignableContractType.java
@@ -0,0 +1,8 @@
+package com.hedvig.backoffice.graphql.resolvers;
+
+public enum SignableContractType {
+  SWEDISH_APARTMENT,
+  SWEDISH_HOUSE,
+  NORWEGIAN_HOME_CONTENT,
+  NORWEGIAN_TRAVEL
+}
--- src/main/java/com/hedvig/backoffice/services/underwriter/dtos/QuoteRequestDto.kt
+++ src/main/java/com/hedvig/backoffice/services/underwriter/dtos/QuoteRequestDto.kt
@@ -1,12 +1,14 @@
 package com.hedvig.backoffice.services.underwriter.dtos
 
+import com.hedvig.backoffice.graphql.types.QuoteInput
+import com.hedvig.backoffice.graphql.types.QuoteInputData
 import com.hedvig.backoffice.services.product_pricing.dto.contract.ExtraBuilding
 import java.time.LocalDate
 import java.util.UUID
 
 data class QuoteRequestDto(
-  val firstName: String,
-  val lastName: String,
+  val firstName: String?,
+  val lastName: String?,
   val currentInsurer: String?,
   val birthDate: LocalDate?,
   val ssn: String?,
@@ -19,7 +21,28 @@ data class QuoteRequestDto(
   val memberId: String? = null,
   val originatingProductId: UUID? = null,
   val underwritingGuidelinesBypassedBy: String?
-)
+) {
+  companion object {
+    fun from(input: QuoteInput, memberId: String, underwritingGuidelinesBypassedBy: String?): QuoteRequestDto {
+      return QuoteRequestDto(
+        firstName = null,
+        lastName = null,
+        currentInsurer = input.currentInsurer,
+        birthDate = null,
+        ssn = null,
+        quotingPartner = null,
+        productType = input.productType?.toString()?.let(ProductType::valueOf),
+        incompleteHouseQuoteData = input.houseData?.let(IncompleteHouseQuoteDataDto::from),
+        incompleteApartmentQuoteData = input.apartmentData?.let(IncompleteApartmentQuoteDataDto::from),
+        norwegianHomeContentsData = input.norwegianHomeContentData?.let(IncompleteNorwegianHomeContentsQuoteDataDto::from),
+        norwegianTravelData = input.norwegianTravelData?.let(IncompleteNorwegianTravelQuoteDataDto::from),
+        memberId = memberId,
+        originatingProductId = null,
+        underwritingGuidelinesBypassedBy = underwritingGuidelinesBypassedBy
+      )
+    }
+  }
+}
 
 data class IncompleteHouseQuoteDataDto(
   val street: String?,
@@ -49,6 +72,21 @@ data class IncompleteHouseQuoteDataDto(
         personalNumber = dto.ssn,
         isSubleted = dto.isSubleted ?: false
       )
+
+    fun from(input: QuoteInputData.HouseQuoteInput): IncompleteHouseQuoteDataDto =
+      IncompleteHouseQuoteDataDto(
+        street = input.street,
+        zipCode = input.zipCode,
+        city = input.city,
+        livingSpace = input.livingSpace,
+        householdSize = input.householdSize,
+        ancillaryArea = input.ancillaryArea,
+        yearOfConstruction = input.yearOfConstruction,
+        numberOfBathrooms = input.numberOfBathrooms,
+        extraBuildings = input.extraBuildings,
+        personalNumber = null,
+        isSubleted = input.isSubleted ?: false
+      )
   }
 }
 
@@ -69,6 +107,16 @@ data class IncompleteApartmentQuoteDataDto(
         livingSpace = dto.livingSpace,
         householdSize = dto.householdSize,
         subType = dto.subType
+      )
+
+    fun from(input: QuoteInputData.ApartmentQuoteInput): IncompleteApartmentQuoteDataDto =
+      IncompleteApartmentQuoteDataDto(
+        street = input.street,
+        zipCode = input.zipCode,
+        city = input.city,
+        livingSpace = input.livingSpace,
+        householdSize = input.householdSize,
+        subType = input.subType
       )
   }
 }
@@ -91,6 +139,16 @@ data class IncompleteNorwegianHomeContentsQuoteDataDto(
         coInsured = dto.coInsured,
         type = dto.type
       )
+
+    fun from(input: QuoteInputData.NorwegianHomeContentQuoteInput): IncompleteNorwegianHomeContentsQuoteDataDto =
+      IncompleteNorwegianHomeContentsQuoteDataDto(
+        street = input.street,
+        zipCode = input.zipCode,
+        city = input.city,
+        livingSpace = input.livingSpace,
+        coInsured = input.householdSize?.minus(1),
+        type = input.type
+      )
   }
 }
 
@@ -101,6 +159,11 @@ data class IncompleteNorwegianTravelQuoteDataDto(
     fun from(dto: QuoteData.NorwegianTravelData): IncompleteNorwegianTravelQuoteDataDto =
       IncompleteNorwegianTravelQuoteDataDto(
         coInsured = dto.coInsured
+      )
+
+    fun from(dto: QuoteInputData.NorwegianTravelQuoteInput): IncompleteNorwegianTravelQuoteDataDto =
+      IncompleteNorwegianTravelQuoteDataDto(
+        coInsured = dto.householdSize?.minus(1)
       )
   }
 }
