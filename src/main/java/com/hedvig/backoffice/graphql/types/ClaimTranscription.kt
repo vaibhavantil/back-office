312d6f26b5921bb6631949c547108cabd40cc662
Fredrik Areschoug <fredrik.areschoug@gmail.com>
2020-05-29 10:46:35 +0200

    added language code to transcription

--- src/main/java/com/hedvig/backoffice/graphql/types/ClaimTranscription.kt
+++ src/main/java/com/hedvig/backoffice/graphql/types/ClaimTranscription.kt
@@ -4,11 +4,12 @@ import com.hedvig.backoffice.services.claims.dto.ClaimTranscription as ClaimTran
 
 data class ClaimTranscription(
   val text: String,
-  val confidenceScore: Float
+  val confidenceScore: Float,
+  val languageCode: String
 ) {
   companion object {
     fun fromDTO(dto: ClaimTranscriptionDTO): ClaimTranscription? {
-      return ClaimTranscription(dto.text, dto.confidenceScore)
+      return ClaimTranscription(dto.text, dto.confidenceScore, dto.languageCode)
     }
   }
 }
--- src/main/java/com/hedvig/backoffice/services/claims/dto/ClaimTranscription.kt
+++ src/main/java/com/hedvig/backoffice/services/claims/dto/ClaimTranscription.kt
@@ -2,5 +2,6 @@ package com.hedvig.backoffice.services.claims.dto
 
 data class ClaimTranscription(
   val text: String,
-  val confidenceScore: Float
+  val confidenceScore: Float,
+  val languageCode: String
 )
