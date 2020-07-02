2de1c2d12430086dbd4ecd96e31e2baa14dff96e
Gustaf H <gustaf.halvardsson@gmail.com>
2020-07-02 14:51:32 +0200

    made member nullable

--- src/main/java/com/hedvig/backoffice/graphql/resolvers/QuestionGroupResolver.kt
+++ src/main/java/com/hedvig/backoffice/graphql/resolvers/QuestionGroupResolver.kt
@@ -3,14 +3,11 @@ package com.hedvig.backoffice.graphql.resolvers
 import com.coxautodev.graphql.tools.GraphQLResolver
 import com.hedvig.backoffice.graphql.GraphQLConfiguration
 import com.hedvig.backoffice.graphql.types.Member
-import com.hedvig.backoffice.graphql.types.Member.Companion.fromDTO
 import com.hedvig.backoffice.graphql.types.questions.QuestionGroupType
 import com.hedvig.backoffice.services.members.MemberService
 import com.hedvig.backoffice.services.personnel.PersonnelService
-import com.hedvig.backoffice.util.Gender
 import graphql.schema.DataFetchingEnvironment
 import org.springframework.stereotype.Component
-import java.time.Instant
 
 @Component
 class QuestionGroupResolver
@@ -21,28 +18,16 @@ class QuestionGroupResolver
   fun getMember(
     QuestionGroup: QuestionGroupType,
     env: DataFetchingEnvironment?
-  ): Member {
+  ): Member? {
     return try {
-      fromDTO(
+      Member.fromDTO(
         memberService.findByMemberId(
           QuestionGroup.memberId,
           GraphQLConfiguration.getIdToken(env, personnelService)
         )
       )
     } catch (e: Exception) {
-      Member(
-        QuestionGroup.memberId,
-        "UNKNOWN",
-        "UNKNOWN",
-        "Unknown",
-        Gender.OTHER,
-        "Unknown",
-        "Unknown",
-        "Unknown",
-        Instant.now(),
-        "Unknown",
-        "Unknown"
-      )
+      null
     }
   }
 
