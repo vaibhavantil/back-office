e9dfb0d07b24b29320df1cbfd879e04564fb6560
Gustaf H <gustaf.halvardsson@gmail.com>
2020-07-02 17:11:44 +0200

    added sentry and logging tracking

--- src/main/java/com/hedvig/backoffice/graphql/resolvers/QuestionGroupResolver.kt
+++ src/main/java/com/hedvig/backoffice/graphql/resolvers/QuestionGroupResolver.kt
@@ -4,16 +4,20 @@ import com.coxautodev.graphql.tools.GraphQLResolver
 import com.hedvig.backoffice.graphql.GraphQLConfiguration
 import com.hedvig.backoffice.graphql.types.Member
 import com.hedvig.backoffice.graphql.types.questions.QuestionGroupType
+import com.hedvig.backoffice.services.chat.ChatServiceV2Impl
 import com.hedvig.backoffice.services.members.MemberService
 import com.hedvig.backoffice.services.personnel.PersonnelService
 import graphql.schema.DataFetchingEnvironment
+import io.sentry.Sentry
+import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Component
 
 @Component
 class QuestionGroupResolver
   (
-  private val memberService: MemberService,
+    private val memberService: MemberService,
   private val personnelService: PersonnelService
+
 ) : GraphQLResolver<QuestionGroupType> {
   fun getMember(
     questionGroup: QuestionGroupType,
@@ -22,15 +26,19 @@ class QuestionGroupResolver
     return try {
       Member.fromDTO(
         memberService.findByMemberId(
-          QuestionGroup.memberId,
+          questionGroup.memberId,
           GraphQLConfiguration.getIdToken(env, personnelService)
         )
       )
     } catch (exception: Exception) {
-      logger.error("Unable get to resolve member for Question Group (memberId=${questionGroup.memberId}), exception)
+      logger.error("Unable get to resolve member for Question Group (memberId=${questionGroup.memberId}), exception");
       Sentry.capture(exception)
       null
     }
+  }
+
+  companion object {
+    private val logger = LoggerFactory.getLogger(ChatServiceV2Impl::class.java)
   }
 
 }
