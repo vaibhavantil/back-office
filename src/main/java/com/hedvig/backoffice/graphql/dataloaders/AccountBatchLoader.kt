4b5c12d391c43c1d18407e4f023bd49e7bb4ca3b
AlexSJ1990 <alex.sinclair.joice@gmail.com>
2020-10-21 16:36:28 +0200

    small fix

--- src/main/java/com/hedvig/backoffice/graphql/dataloaders/AccountBatchLoader.kt
+++ src/main/java/com/hedvig/backoffice/graphql/dataloaders/AccountBatchLoader.kt
@@ -8,8 +8,8 @@ import java.util.concurrent.CompletableFuture
 import java.util.concurrent.CompletionStage
 
 @Component
-class AccountBatchLoader(private val accountService: AccountService) : BatchLoader<String, Account?> {
-  override fun load(memberIds: List<String>): CompletionStage<List<Account?>> {
+class AccountBatchLoader(private val accountService: AccountService) : BatchLoader<String?, Account?> {
+  override fun load(memberIds: List<String?>): CompletionStage<List<Account?>> {
     return CompletableFuture.supplyAsync {
         accountService.batchFindCurrentBalances(memberIds)
             .map { accountDto -> accountDto?.let { Account.from(accountDto) } }
