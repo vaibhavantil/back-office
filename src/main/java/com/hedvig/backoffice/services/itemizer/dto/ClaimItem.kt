efb945214fe42118b42d427c6d8e106d958ed625
vonElfvin <elvin.granat@gmail.com>
2021-02-18 11:08:23 +0100

    Resolve a ClaimItemSet on the graphql Claim

--- src/main/java/com/hedvig/backoffice/services/itemizer/dto/ClaimItem.kt
+++ src/main/java/com/hedvig/backoffice/services/itemizer/dto/ClaimItem.kt
@@ -1,20 +1,18 @@
 package com.hedvig.backoffice.services.itemizer.dto
 
 import com.hedvig.graphql.commons.type.MonetaryAmountV2
-import java.math.BigDecimal
 import java.time.LocalDate
-import java.util.*
-import javax.money.MonetaryAmount
+import java.util.UUID
 
 data class ClaimItem(
-  val id: UUID,
-  val itemFamily: ItemFamily,
-  val itemType: ItemType,
-  val itemBrand: ItemBrand?,
-  val itemModel: ItemModel?,
-  val itemCompany: ItemCompany?,
-  val dateOfPurchase: LocalDate?,
-  val purchasePrice: MonetaryAmountV2?,
-  val valuation: MonetaryAmountV2?,
-  val note: String?
+    val id: UUID,
+    val itemFamily: ItemFamily,
+    val itemType: ItemType,
+    val itemBrand: ItemBrand?,
+    val itemModel: ItemModel?,
+    val itemCompany: ItemCompany?,
+    val dateOfPurchase: LocalDate?,
+    val purchasePrice: MonetaryAmountV2?,
+    val valuation: MonetaryAmountV2?,
+    val note: String?
 )
