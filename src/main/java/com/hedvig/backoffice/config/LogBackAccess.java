dbb03dc5d6a38c2be37cdee1ab2139b81cba05af
Fredrik Tõnisson-Bystam <fredrik.bystam@hedvig.com>
2021-03-26 09:02:54 +0100

    get rid of (unused) TeeFilter to avoid stack trace vomit

--- src/main/java/com/hedvig/backoffice/config/LogBackAccess.java
+++ src/main/java/com/hedvig/backoffice/config/LogBackAccess.java
@@ -1,18 +1,12 @@
 package com.hedvig.backoffice.config;
 
 import ch.qos.logback.access.tomcat.LogbackValve;
-import javax.servlet.Filter;
 import org.apache.catalina.Valve;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class LogBackAccess {
-
-  @Bean(name = "TeeFilter")
-  public Filter teeFilter() {
-    return new ch.qos.logback.access.servlet.TeeFilter();
-  }
 
   @Bean
   public Valve logbackValve() {
